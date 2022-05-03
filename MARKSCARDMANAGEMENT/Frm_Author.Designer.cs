b97ccfe5526d1c60bacce2a23ae6fa440c35ec23
Sumanth Gowda <drgowda6@gmail.com>
2022-05-03 23:18:30 +0530

    New Update 03_05_2022

--- MARKSCARDMANAGEMENT/Frm_Author.Designer.cs
+++ MARKSCARDMANAGEMENT/Frm_Author.Designer.cs
@@ -30,58 +30,19 @@ namespace MARKSCARDMANAGEMENT
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Author));
-            this.lbl_name = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.picbx = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picbx)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lbl_name
-            // 
-            this.lbl_name.AutoSize = true;
-            this.lbl_name.BackColor = System.Drawing.Color.White;
-            this.lbl_name.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_name.ForeColor = System.Drawing.Color.Red;
-            this.lbl_name.Location = new System.Drawing.Point(283, 142);
-            this.lbl_name.Name = "lbl_name";
-            this.lbl_name.Size = new System.Drawing.Size(580, 45);
-            this.lbl_name.TabIndex = 17;
-            this.lbl_name.Text = "Seshadripuram College Tumakuru";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(436, 380);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 23);
-            this.label1.TabIndex = 18;
-            this.label1.Text = "Concept :";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(431, 412);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 23);
-            this.label2.TabIndex = 19;
-            this.label2.Text = "Architect :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(419, 446);
+            this.label3.Location = new System.Drawing.Point(411, 305);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(111, 23);
             this.label3.TabIndex = 20;
@@ -92,7 +53,7 @@ namespace MARKSCARDMANAGEMENT
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.label4.Location = new System.Drawing.Point(417, 330);
+            this.label4.Location = new System.Drawing.Point(415, 171);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(115, 33);
             this.label4.TabIndex = 21;
@@ -102,31 +63,11 @@ namespace MARKSCARDMANAGEMENT
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(537, 446);
+            this.label5.Location = new System.Drawing.Point(529, 305);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(349, 23);
+            this.label5.Size = new System.Drawing.Size(126, 23);
             this.label5.TabIndex = 24;
-            this.label5.Text = "Sumanth D R (Student 2017-20 Batch)";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(537, 412);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(254, 23);
-            this.label6.TabIndex = 23;
-            this.label6.Text = "Prof.Nagabushan Hiremath";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(537, 380);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(354, 23);
-            this.label7.TabIndex = 22;
-            this.label7.Text = "Prof.Prathibha S B (HOD Dept. of BCA)";
+            this.label5.Text = "Sumanth D R";
             // 
             // label8
             // 
@@ -134,26 +75,16 @@ namespace MARKSCARDMANAGEMENT
             this.label8.BackColor = System.Drawing.Color.White;
             this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.LightSeaGreen;
-            this.label8.Location = new System.Drawing.Point(418, 239);
+            this.label8.Location = new System.Drawing.Point(334, 66);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(483, 37);
             this.label8.TabIndex = 25;
             this.label8.Text = "STUDENT MANAGEMENT SYSTEM";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(488, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(139, 127);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 16;
-            this.pictureBox1.TabStop = false;
-            // 
             // picbx
             // 
             this.picbx.Image = ((System.Drawing.Image)(resources.GetObject("picbx.Image")));
-            this.picbx.Location = new System.Drawing.Point(12, 239);
+            this.picbx.Location = new System.Drawing.Point(61, 145);
             this.picbx.Name = "picbx";
             this.picbx.Size = new System.Drawing.Size(318, 249);
             this.picbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -167,21 +98,14 @@ namespace MARKSCARDMANAGEMENT
             this.ClientSize = new System.Drawing.Size(1140, 537);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lbl_name);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.picbx);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Frm_Author";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picbx)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -191,15 +115,9 @@ namespace MARKSCARDMANAGEMENT
         #endregion
 
         private System.Windows.Forms.PictureBox picbx;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label lbl_name;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
     }
 }
